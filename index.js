7837073bc0e31c44a12c041d53890f0f6ac460c8
AFCMS <afcm.contact@gmail.com>
2021-11-17 21:35:10 +0100

    fix5

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (!config.token) {
 
 console.log(config.token);
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 
 let mentionString = "";
 
