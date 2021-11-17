d71658f255da21665954b1c4f502cc0c6825d1eb
AFCMS <afcm.contact@gmail.com>
2021-11-17 21:16:50 +0100

    fix2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const Discord = require("discord.js");
+const {token} = require("./config.json")
+
+// Error if missing configuration
+if (!token) {
+	console.log("Error: Missing configurations! See config.json.example.");
+	return;
+}
