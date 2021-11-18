884e24e46ea2d7f8c100679405262c5587df022b
AFCMS <afcm.contact@gmail.com>
2021-11-18 15:49:53 +0100

    fix

--- index2.js
+++ index2.js
@@ -22,18 +22,27 @@ const client = new Discord.Client({
 });
 
 
+client.once("ready", () => {
+	console.log(`Logged in as ${client.user.tag}.`);
+
+	const CLIENT_ID = client.user.id
+	mentionString = `<@!${client.user.id}>`
+
+	// Modify bot activity
+	client.user.setActivity("no one.", {type: "LISTENING"});
+
+	const commands = [
+		new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+		new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+		new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	]
+		.map(command => command.toJSON());
+	
+	const rest = new REST({ version: '9' }).setToken(config.token);
+	
+	rest.put(Routes.applicationGuildCommands(client.user.id, config.guildid), { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);
+});
+
 client.login(config.token);
-
-
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
-
-const rest = new REST({ version: '9' }).setToken(config.token);
-
-rest.put(Routes.applicationGuildCommands(client.user.id, config.guildid), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
