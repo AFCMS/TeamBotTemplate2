ab5cf33ef8bd126c2c09c57ee8bcbfb1a6007bbf
AFCMS <afcm.contact@gmail.com>
2021-11-18 20:25:21 +0100

    fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const { REST } = require("@discordjs/rest");
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Routes } = require("discord-api-types/v9");
 const Discord = require("discord.js");
 const config = require('./config.json')
 
 // Error if missing configuration
-if (!config.token) {
+if (!config.token && !config.guildid) {
 	console.log("Error: Missing configurations! See config.json.example.");
 	return;
 }
@@ -20,74 +21,21 @@ const client = new Discord.Client({
 	]
 });
 
-
-// Import all Commands
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-
 const commands = [];
 
-client.commands = new Discord.Collection();
+commands.push(
+	new SlashCommandBuilder()
+		.setName('team')
+		.setDescription('A dummy command'),
+);
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-	client.commands.set(command.data.name, command);
-}
-
-let mentionString = "";
 
 client.once("ready", () => {
 	console.log(`Logged in as ${client.user.tag}.`);
 
-	const CLIENT_ID = client.user.id
-	mentionString = `<@!${client.user.id}>`
-
-	// Modify bot activity
-	client.user.setActivity("no one.", {type: "LISTENING"});
-
-	// Register Commands
-	const rest = new REST({
-		version: "9"
-	}).setToken("Bot "+config.token);
-
-	(async () => {
-		try {
-			//await rest.put(Routes.applicationCommand(CLIENT_ID), {
-			//	body: commands
-			//})
-			await rest.put(Routes.applicationGuildCommands(CLIENT_ID, config.guildid), {
-				body: commands
-			});
-			console.log("Commands Registered!");
-		} catch (err) {
-			console.log(err);
-		}
-	})();
-});
-
-// Execute Commands then needed
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	for (const command of commands) {
+		client.api.applications(client.user.id).guilds(config.guildid).commands.post({data: command})
 	}
 });
-
-client.on("messageCreate", async message => {
-	if (message.content === mentionString) {
-		message.reply(`Ping!`);
-		return;
-	}
-	//message.channel.send(`Hello!`);
-});
-
 
 client.login(config.token);
