011d25f00970500f9a083a8da8f7ef21d23228f5
AFCMS <afcm.contact@gmail.com>
2021-12-31 12:01:23 +0100

    fix

--- index.js
+++ index.js
@@ -69,17 +69,17 @@ commands.push(
 commands.push(new SlashCommandBuilder()
 	.setName('info')
 	.setDescription('Get info about a user or a server!')
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('user')
-			.setDescription('Info about a user')
-			.addUserOption(option => option.setName('target').setDescription('The user'))
-		)
-	.addSubcommand(subcommand =>
-		subcommand
-			.setName('server')
-			.setDescription('Info about the server')
-		)
+	//.addSubcommand(subcommand =>
+	//	subcommand
+	//		.setName('user')
+	//		.setDescription('Info about a user')
+	//		.addUserOption(option => option.setName('target').setDescription('The user'))
+	//	)
+	//.addSubcommand(subcommand =>
+	//	subcommand
+	//		.setName('server')
+	//		.setDescription('Info about the server')
+	//	)
 );
 
 commands.push(
